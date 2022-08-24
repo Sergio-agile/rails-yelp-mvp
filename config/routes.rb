412e576dc0c49ae5849e5e744c4b333eb170721e
sergio garcia <sergio.garcia.sanjuan@gmail.com>
2022-08-24 16:39:23 +0200

    passing all test after refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'review/new'
-  get 'review/create'
-  # get 'restaurants/index'
-  # get 'restaurants/new'
-  # get 'restaurants/create'
-  # get 'restaurants/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
